98c3974a099893cd3ebb8fd6ee4ca0ec3fcb4747
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-08-06 15:59:11 +0600

    Add

--- SPRING/Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/repository/DoctorDiscountRepository.java
+++ SPRING/Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/repository/DoctorDiscountRepository.java
@@ -3,6 +3,7 @@ package emranhss.com.Modern_Hospital_Management_System.repository;
 import emranhss.com.Modern_Hospital_Management_System.entity.DoctorDiscount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DoctorDiscountRepository extends JpaRepository<DoctorDiscount, Long> {
@@ -10,4 +11,6 @@ public interface DoctorDiscountRepository extends JpaRepository<DoctorDiscount, 
     Optional<DoctorDiscount> findByDoctorId(Long doctorId);
 
     Optional<DoctorDiscount> findByDoctorIdAndActiveTrue(Long doctorId);
+
+    List<DoctorDiscount> findByActiveTrue();
 }
