d997666e99169f54cf940d992e9a614689a73217
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-08-29 15:53:34 +0600

    Add

--- Flutter_Hospital_Management/lib/core/pdf/pdf_export_web.dart
+++ Flutter_Hospital_Management/lib/core/pdf/pdf_export_web.dart
@@ -1,10 +1,11 @@
+// ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
 import 'dart:html' as html;
 import 'dart:typed_data';
 
 Future<void> exportPdf(Uint8List bytes, String filename) async {
   final blob = html.Blob([bytes], 'application/pdf');
   final url = html.Url.createObjectUrlFromBlob(blob);
-  final anchor = html.AnchorElement()
+  html.AnchorElement()
     ..href = url
     ..setAttribute('download', filename)
     ..click();
